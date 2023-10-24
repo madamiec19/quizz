7062e92cb758e8d1dca170ceb7dc3cf3bcb4c523
madamiec19 <madamiec19@gmail.com>
2023-10-24 22:56:54 +0200

    rebuild

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:quizz/QuizGame.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
-
-QuizGame quizGame = QuizGame();
-
 
 class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
         body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
             child: QuizPage(),
           ),
         ),
@@ -30,26 +29,36 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
 
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain.getCorrectAnswer();
 
-  @override
-  Widget build(BuildContext context) {
-    List<Icon> scoreKeeper = [];
+    setState(() {
+      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
+      //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
+      //HINT! Step 4 Part B is in the quiz_brain.dart
+      //TODO: Step 4 Part C - reset the questionNumber,
+      //TODO: Step 4 Part D - empty out the scoreKeeper.
 
-    for (var result in quizGame.scoreKeeper){
-      if(result == true){
+      //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
+      if (userPickedAnswer == correctAnswer) {
         scoreKeeper.add(const Icon(
           Icons.check,
           color: Colors.green,
         ));
       } else {
         scoreKeeper.add(const Icon(
-          Icons.cancel,
+          Icons.close,
           color: Colors.red,
         ));
       }
-    }
+      quizBrain.nextQuestion();
+    });
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -57,10 +66,10 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           flex: 5,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quizGame.getActualQuestionText(),
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: const TextStyle(
                   fontSize: 25.0,
@@ -73,44 +82,39 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: const EdgeInsets.all(15.0),
-            child: Container(
-              color: Colors.green,
-              child: TextButton(
-                child: const Text(
-                  'True',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                  ),
+            child: TextButton(
+              style: TextButton.styleFrom(textStyle: const TextStyle(color: Colors.white), 
+              backgroundColor: Colors.green),         
+              child: const Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
                 ),
-                onPressed: () {
-                  setState(() {
-                    quizGame.checkQuestion(true);
-                  });
-                },
               ),
+              onPressed: () {
+                //The user picked true.
+                checkAnswer(true);
+              },
             ),
           ),
         ),
         Expanded(
           child: Padding(
             padding: const EdgeInsets.all(15.0),
-            child: Container(
-              color: Colors.red,
-              child: TextButton(
-                child: const Text(
-                  'False',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.red),
+              child: const Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
                 ),
-                onPressed: () {
-                  setState(() {
-                    quizGame.checkQuestion(false);
-                  });
-                },
               ),
+              onPressed: () {
+                //The user picked false.
+                checkAnswer(false);
+              },
             ),
           ),
         ),
